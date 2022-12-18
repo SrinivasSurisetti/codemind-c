e2aabb45b3f3d5c9de3b7dc43d4948f6ca6e8b0c
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2022-12-18 19:38:56 +0530

    #include<stdio.h> int main() {     char s;     scanf("%c",&s);     if(s=='V')     {         printf("Violet");     }     else if(s=='I')     {         printf("Indigo");     }     else if(s=='B')     {         printf("Blue");     }     else if(s=='G')     {         printf("Green");     }     else if(s=='Y')     {         printf("Yellow");     }     else if(s=='R')     {         printf("Red");     }     else     {         printf("%d",-1);     } }

---
+++ Print_the_color_name.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    char s;
+    scanf("%c",&s);
+    if(s=='V')
+    {
+        printf("Violet");
+    }
+    else if(s=='I')
+    {
+        printf("Indigo");
+    }
+    else if(s=='B')
+    {
+        printf("Blue");
+    }
+    else if(s=='G')
+    {
+        printf("Green");
+    }
+    else if(s=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(s=='R')
+    {
+        printf("Red");
+    }
+    else
+    {
+        printf("%d",-1);
+    }
+}
