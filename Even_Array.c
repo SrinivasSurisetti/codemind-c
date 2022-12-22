c7ce8819c0b7ffd33a4c58f07ae68d01521e256d
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2022-12-22 22:59:23 +0530

    #include<stdio.h> int main() {     int i,a[100],c=0,n;     scanf("%d",&n);     //int a[i];     for(i=1;i<=n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<=n;i++)     {         if(a[i]%2==0)         {             c++;         }     }     if(c==n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Even_Array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int i,a[100],c=0,n;
+    scanf("%d",&n);
+    //int a[i];
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            c++;
+        }
+    }
+    if(c==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
