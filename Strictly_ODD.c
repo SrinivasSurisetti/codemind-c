76d864250bde23abacebc0e3142b6411a13cf7f7
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2022-12-30 22:31:30 +0530

    #include<stdio.h> int main() {     int i,n,a[n],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==1)         {             c=0;             if(i%2!=1)             {             c++;             break;             }         }     }     if(c==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[n],c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            c=0;
+            if(i%2!=1)
+            {
+            c++;
+            break;
+            }
+        }
+    }
+    if(c==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
