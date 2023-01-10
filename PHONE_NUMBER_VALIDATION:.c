c5af21207d10266535f8b6822f41855531f77482
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2023-01-10 11:19:10 +0530

    #include<stdio.h> int main() {     long long int n;     scanf("%lld",&n);     if(n<=9999999999 && n>1000000000)     {         printf("Valid");     }     else     {         printf("Invalid");     } }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    long long int n;
+    scanf("%lld",&n);
+    if(n<=9999999999 && n>1000000000)
+    {
+        printf("Valid");
+    }
+    else
+    {
+        printf("Invalid");
+    }
+}
