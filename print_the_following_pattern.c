a3c754e369a973ee9b2acf399027f1fa0263482b
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2023-05-19 21:22:31 +0530

    #include<stdio.h>
    int main() {
        int n;
        scanf("%d",&n);
                for(int i =1;i<=n;i++) {
                    for(int j=1;j<=n;j++) {
                        if(i==j) printf("0");
                        else printf("x");
                    }
                    printf("
    ");
                }
        }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,8 +5,8 @@ int main() {
     scanf("%d",&n);
             for(int i =1;i<=n;i++) {
                 for(int j=1;j<=n;j++) {
-                    if(i==j || j+i==n+1) printf("x");
-                    else printf("0");
+                    if(i==j) printf("0");
+                    else printf("x");
                 }
                 printf("
 ");
