12558c0375aacf181f98b19988b81282238747ea
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2023-05-22 20:11:58 +0530

    #include<stdio.h>
    int dc(int n)
    {
        int r,k=0;
        while(n>0)
        {
            r=n%10;
            k++;
            n=n/10;
        }
        return k;
    }
    int main()
    {
        int n,r;
        scanf("%d",&n);
        int k=dc(n);
        int x[k];
        for(int i=k-1;i>=0;i--)
        {
            r=n%10;
            x[i]=r;
            n=n/10;
        }
        for(int i=0;i<k;i++)
        {
            if(x[i]!=9)
            {
                x[i]=9;
                break;
            }
        }
        for(int i=0;i<k;i++)
        {
            printf("%d",x[i]);
        }
    }

---
+++ Maximum_69_Number.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int dc(int n)
+{
+    int r,k=0;
+    while(n>0)
+    {
+        r=n%10;
+        k++;
+        n=n/10;
+    }
+    return k;
+}
+int main()
+{
+    int n,r;
+    scanf("%d",&n);
+    int k=dc(n);
+    int x[k];
+    for(int i=k-1;i>=0;i--)
+    {
+        r=n%10;
+        x[i]=r;
+        n=n/10;
+    }
+    for(int i=0;i<k;i++)
+    {
+        if(x[i]!=9)
+        {
+            x[i]=9;
+            break;
+        }
+    }
+    for(int i=0;i<k;i++)
+    {
+        printf("%d",x[i]);
+    }
+}
