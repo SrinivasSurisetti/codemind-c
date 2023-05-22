c27520cc745e3a1394f4ae32b8c186349fee16f9
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2023-05-22 20:04:28 +0530

    #include<stdio.h>
    int add(int n)
    {
        int r,s=0;
        while(n>0)
        {
            r=n%10;
            s=s+r;
            n=n/10;
        }
        return s;
    }
    int main()
    {
        int n;
        scanf("%d",&n);
        int s,r;
        do
        {
            s=add(n);
            n=s;
        }while(n>9);
        printf("%d",n);
    }

---
+++ Add_Digits.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int add(int n)
+{
+    int r,s=0;
+    while(n>0)
+    {
+        r=n%10;
+        s=s+r;
+        n=n/10;
+    }
+    return s;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int s,r;
+    do
+    {
+        s=add(n);
+        n=s;
+    }while(n>9);
+    printf("%d",n);
+}
