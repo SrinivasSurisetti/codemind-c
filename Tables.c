a429f8efbbf09da701075324a725b559cbfe1a0c
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2023-06-04 22:43:45 +0530

    #include<stdio.h> int main() {     int n,m,i,o;     scanf("%d%d",&n,&m);     for(i=1;i<=m;i++)     {         if(i%2==1)     {         o=n*i;         printf("%d x %d = %d ",n,i,o);     }     }
    }

--- Tables.c
+++ Tables.c
@@ -3,14 +3,14 @@ int main()
 {
     int n,m,i,o;
     scanf("%d%d",&n,&m);
-    for(i=1;i<=m;i=i+2)
+    for(i=1;i<=m;i++)
     {
-    
-    
+        if(i%2==1)
+    {
         o=n*i;
         printf("%d x %d = %d
 ",n,i,o);
-    
+    }
     }
 
 }
